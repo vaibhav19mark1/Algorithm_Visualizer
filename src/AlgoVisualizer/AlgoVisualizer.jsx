76a31492bcafe9980594ca92d0802f1fc37af6c2
vaibhav19mark1 <vaibhavtushar19@gmail.com>
2022-03-24 22:26:01 +0530

    initialized grid

---
+++ src/AlgoVisualizer/AlgoVisualizer.jsx
@@ -0,0 +1,89 @@
+// Importing the required components
+import React, { Component } from "react";
+import Node from "./Node/Node";
+import {dijkstra,getShortestPath} from '../algorithms/dijkstra';
+
+// Importing CSS file
+import "./AlgoVisualizer.css";
+
+const START_NODE_ROW = 10;
+const START_NODE_COL = 5;
+const FINISH_NODE_ROW = 10;
+const FINISH_NODE_COL = 45;
+
+export default class AlgoVisualizer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: [],
+    };
+  }
+
+  componentDidMount() {
+    const grid=getInitialGrid();
+    this.setState({grid});
+  }
+
+  // visualizeDijkstra(){
+  //   const {grid}=this.state;
+  //   startNode=grid[START_NODE_ROW][START_NODE_COL];
+  //   finishNode=grid[FINISH_NODE_ROW][FINISH_NODE_COL];
+  //   visitedNodesInOrder=dijkstra(grid,startNode,finishNode);
+  //   nodesInShortestPath=getShortestPath(finishNode);
+  // }
+
+  render() {
+    const { grid } = this.state;
+    console.log(grid);
+    return (
+      <>
+        {/* <button onClick={visualizeDijkstra()}>
+          Visualize Dijkstra's Algo
+        </button> */}
+        <div className="grid">
+          {grid.map((row, rowIdx) => {
+            return (
+              <div key={rowIdx}>
+                {row.map((node, nodeIdx) => {
+                  const { isStart, isFinish } = node;
+                  return (
+                    <Node
+                      key={nodeIdx}
+                      isStart={isStart}
+                      isFinish={isFinish}
+                    ></Node>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
+}
+
+const getInitialGrid = () => {
+  const grid = [];
+  for (let row = 0; row < 20; row++) {
+    const currentRow = [];
+    for (let col = 0; col < 50; col++) {
+      currentRow.push(createNode(row,col));
+    }
+    grid.push(currentRow);
+  }
+  return grid;
+};
+
+const createNode = (row, col) => {
+  return {
+    row,
+    col,
+    isStart: row === START_NODE_ROW && col === START_NODE_COL,
+    isFinish: row === FINISH_NODE_ROW && col === FINISH_NODE_COL,
+    distance: Infinity,
+    isVisited: false,
+    isWall: false,
+    previousNode: null,
+  };
+};
