0ce84204afeba0153d05437f9e290f20f0da546a
vaibhav19mark1 <vaibhavtushar19@gmail.com>
2022-09-20 00:11:32 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.css';
+import '../node_modules/bootstrap/js/dist/dropdown';
+import AlgoVisualizer from './AlgoVisualizer/AlgoVisualizer';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AlgoVisualizer />
   </React.StrictMode>,
   document.getElementById('root')
 );
